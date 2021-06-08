19c38185fa8bb3e199c68d227df98f26594b70de
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2021-06-08 23:35:57 +0600

    String is completed

---
+++ Class1/string.py
@@ -0,0 +1,13 @@
+name = 'Ada Lovelace'
+print(name.upper())
+print(name.lower())
+
+# name1 = 'Mehedi'
+# name2 = 'mehedi'
+firstName = 'Mehedi'
+lastName = ' Hasan'
+
+fullName = firstName + lastName
+
+print(fullName) #MehediHasan , #Mehedi Hasan
+
