cd46b4c5b10e1472aeaaec958cbcad15ef6576e9
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2021-06-11 00:15:24 +0600

    introduction to list

---
+++ Class2/whatIsLIist.py
@@ -0,0 +1,8 @@
+
+# a list is collection of items in a particular order
+
+names = ['Mehedi','sifat','Mahadi','khairul','Israt','hasnat','Nahid',0,1,2,3]
+
+print('I am '+names[2]+' I have '+str(names[-1])+' points')
+
+
