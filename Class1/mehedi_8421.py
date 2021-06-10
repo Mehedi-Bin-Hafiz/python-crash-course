c1b4d820fdd7ee47adbe71629bd97ed19dec48d0
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2021-06-11 00:00:18 +0600

    completed class 1

---
+++ Class1/mehedi_8421.py
@@ -0,0 +1,13 @@
+
+a = 'hello'
+b = 'world'
+c = a + b
+print(c)
+
+c = a + ' ' + b
+print(c)
+
+a = 5
+b ='jhon'
+
+print(type(a), type(b))
---
+++ Class1/number.py
@@ -0,0 +1,27 @@
+#integer
+
+
+a, b = 2, 3
+
+print(a-b)
+print(b+a)
+print(a*b)
+print(b/a)
+
+# ** = power
+print(a**b)
+print(b//a)
+
+#float
+
+a , b = 0.1 , 0.1
+
+print(a-b)
+print(b+a)
+print(a*b)
+print(b/a)
+
+c = 2
+print(type(a+c))
+
+
