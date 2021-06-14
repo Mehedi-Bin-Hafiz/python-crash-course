a6de02bebe0c3561b56540f900623360c2d8bbc0
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2021-06-14 23:46:48 +0600

    class 3 is started

---
+++ Class3/looping.py
@@ -0,0 +1,14 @@
+
+studentNames = ['Mehedi','sifat','Mahadi','khairul','Israt','hasnat','Nahid']
+print(studentNames)
+
+# print(studentNames[0])
+# print(studentNames[1])
+# print(studentNames[2])
+# print(studentNames[3])
+
+print('all students:\n')
+n = 1
+for student in studentNames:
+    print(n,'.',student)
+    n = n+1
