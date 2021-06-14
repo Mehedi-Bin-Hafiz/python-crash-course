fcef6eeb389a0999847c7feb65b3d3e625f529ca
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2021-06-14 23:21:12 +0600

    class work solved

---
+++ Class2/classWork.py
@@ -0,0 +1,11 @@
+mylis = [999,1111,3333,4444,5555]
+        #0    1    2    3    4
+
+print(mylis[2] + mylis[4])
+print(mylis[2] - mylis[4])
+print(mylis[2] * mylis[4])
+print(mylis[2] / mylis[4])
+
+print(mylis[1] // mylis[3])
+print(mylis[1] ** mylis[3])
+
