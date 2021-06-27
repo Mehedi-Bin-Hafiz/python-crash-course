31094b3e20558ff2e7360159a0518bda29bb4597
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2021-06-27 23:05:14 +0600

    introduction to while loop and class 7

---
+++ Class6/whileLoop.py
@@ -0,0 +1,13 @@
+
+# i = 0
+# while i < 10:
+#     print(i)
+#     i = i+1
+
+var = True
+i = 0
+while var:
+    name = input('Enter student name: ')
+    print(name)
+    if name == 'mehedi':
+        var = False
