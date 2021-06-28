89f865ab8d9090919927277938c6bc0d08662d90
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2021-06-28 23:57:29 +0600

    while loop complete

--- Class6/whileLoop.py
+++ Class6/whileLoop.py
@@ -4,10 +4,55 @@
 #     print(i)
 #     i = i+1
 
-var = True
-i = 0
-while var:
-    name = input('Enter student name: ')
-    print(name)
-    if name == 'mehedi':
-        var = False
+# var = True
+# i = 0
+# while var:
+#     name = input('Enter student name: ')
+#     print(name)
+#     if name == 'mehedi':
+#         var = False
+
+# studentNames = ['Mehedi','sifat','Mahadi','khairul','Israt','hasnat','Nahid','hasnat','hasnat']
+
+# studentNames.remove('hasnat')
+# studentNames.remove('hasnat')
+# studentNames.remove('hasnat')
+# print(studentNames)
+
+# while 'hasnat' in studentNames:
+#     studentNames.remove('hasnat')
+# print(studentNames)
+
+# studentNames = ['Mehedi','sifat','Mahadi','khairul','Israt','hasnat','Nahid',]
+#
+# # studentNames.append('karim')
+# # studentNames.append('rahim')
+# # studentNames.append('abc')
+# # studentNames.append('bcd')
+# # print(studentNames)
+#
+# decision = True
+# while decision:
+#     name = input('Enter sudent name:')
+#     studentNames.append(name)
+#     ans = input('Do you want to enroll another student? (Yes/No)').upper()
+#     if ans == 'NO':
+#         decision = False
+#     elif ans == "YES":
+#         continue
+# print(studentNames)
+
+studentNamesDict = { 'Mehedi': 24, 'sifat': 23,'Mahadi':23,'khairul':24,'Israt':21,'hasnat':22,'Nahid':20 }
+
+
+decision = True
+while decision:
+    name = input('Enter student name:')
+    age = int(input("Enter student's age: "))
+    studentNamesDict[name] = age
+    ans = input('Do you want to enroll another student? (Yes/No)').upper()
+    if ans == 'NO':
+        decision = False
+    elif ans == "YES":
+        continue
+print(studentNamesDict)
